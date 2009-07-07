8b4d3aea1ce31c2ec6439c254d909330b27fc8ab
Paulo Monteiro <paulo.from.portugal@gmail.com>
2009-07-07 19:46:35 +0100

    First Commit

---
+++ simpleWebApp/src/main/java/com/monteiro/maven/web/HistoryController.java
@@ -0,0 +1,50 @@
+package com.monteiro.maven.web;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.monteiro.maven.weather.model.Location;
+import com.monteiro.maven.weather.model.Weather;
+import com.monteiro.maven.weather.persist.LocationDAO;
+import com.monteiro.maven.weather.persist.WeatherDAO;
+
+public class HistoryController implements Controller {
+
+	private LocationDAO locationDAO;
+	private WeatherDAO weatherDAO;
+
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String zip = request.getParameter("zip");
+		Location location = locationDAO.findByZip(zip);
+		List<Weather> weathers = weatherDAO.recentForLocation( location );
+		
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put( "location", location );
+		model.put( "weathers", weathers );
+		
+		return new ModelAndView("history", model);
+	}
+
+	public WeatherDAO getWeatherDAO() {
+		return weatherDAO;
+	}
+
+	public void setWeatherDAO(WeatherDAO weatherDAO) {
+		this.weatherDAO = weatherDAO;
+	}
+
+	public LocationDAO getLocationDAO() {
+		return locationDAO;
+	}
+
+	public void setLocationDAO(LocationDAO locationDAO) {
+		this.locationDAO = locationDAO;
+	}
+}
